0ca349030221a7d6103e511ada409e0effeb2e4f
Tsuke <00001098102721sp@al.educacao.sp.gov.br>
2024-11-26 14:25:43 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,27 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'GTA San Andreas',
+    'Quem é "CJ"?',
+    'Carl Johnson (mais conhecido pelas suas iniciais "CJ") é um personagem fictício da franquia de jogos eletrônicos Grand Theft Auto, servindo como o protagonista do quinto título principal da série, Grand Theft Auto: San Andreas (2004).
+'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'GTA 4',
+    'quem é o protagonista de GTA 4?',
+    'Niko Bellic um veterano das Guerras Iugoslavas no Leste Europeu, viaja para Liberty City à procura do Sonho Americano e para encontrar o homem que traiu sua unidade do exército quinze anos antes.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'GTA 5',
+    'Quem são os protagonistas de GTA 5',
+    'Franklin Clinton é um personagem da série Grand Theft Auto que aparece como um dos três protagonistas em Grand Theft Auto V, junto com Michael De Santa e Trevor Philips, e um personagem principal em Grand Theft Auto Online como parte da atualização The Contract.'
+    'Michael De Santa (anteriormente Townley) é um dos três personagens principais de GTA V. Ele é um ex-assaltante de banco do meio-oeste que conseguiu um acordo de proteção de testemunhas com o FBI e se aposentou da vida criminosa para morar em Los Santos.'
+'Trevor é um criminoso conhecido e ex-ladrão de banco com um passado complicado, que mais tarde fundou sua própria empresa, Trevor Philips Enterprises, operando com tráfico de drogas e contrabando de armas no Blaine County.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'GTA 6 ',
+    'Quem são os protagonistas de GTA 6',
+    'Lucia tem sido um nome bastante comentado entre os fãs nos últimos meses, já que será a primeira protagonista mulher da franquia GTA em sua era moderna.'
+    'Jason. Os vazamentos do ano passado descrevem Jason como um homem "bastante jovem" que supostamente é interpretado pelo ator Bryan Zampella — o astro, inclusive, tem deixado dicas bem sutis sobre seu envolvimento com GTA 6.'
 )
